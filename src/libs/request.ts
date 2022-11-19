9bf325e42d30b4a4425c5cd2ea9cc1bf5fda8544
giangann <70595717+giangann@users.noreply.github.com>
2022-11-19 23:21:40 +0700

    Change base url

--- src/libs/request.ts
+++ src/libs/request.ts
@@ -1,7 +1,7 @@
 import Axios from 'axios'
 
-export const baseURL = 'http://127.0.0.1:8000/api/'
-
+// export const baseURL = 'http://127.0.0.1:8000/api/'
+export const baseURL = 'https://ruby-coin-api.herokuapp.com/'
 // const refetchTokenURL = ${baseURL}/${V1}/user/refresh-token
 
 async function authRequestInterceptor(config: any) {
